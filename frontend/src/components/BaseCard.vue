8134e9df93f7a6e1557a3793a225568373e9d12e
fyusuf-a <fyusuf-a@student.42.fr>
2023-01-22 17:32:47 +0100

    refactor(frontend): adds a BaseCard component

---
+++ frontend/src/components/BaseCard.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-6 font-dark bg-light rounded-md shadow-md">
+    <slot></slot>
+  </div>
+</template>
--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -3,6 +3,7 @@ import { onBeforeMount, ref } from 'vue';
 import { useMatchaStore } from '@/store';
 import axios from 'axios';
 import { useRouter } from 'vue-router';
+import BaseCard from '@/components/BaseCard.vue';
 
 const store = useMatchaStore();
 
@@ -27,18 +28,20 @@ async function login() {
         <h1 class="text-2xl text-center pb-12">Sign in to Matcha</h1>
       </header>
       <main>
-        <form class="mx-auto p-4 bg-light font-darker">
-          <div class="form-field">
-            <label for="username">Username</label>
-            <input type="text" id="username" v-model.trim="userName" />
-          </div>
-          <div class="form-field">
-            <label for="password">Password</label>
-            <input type="password" id="password" v-model="password" />
-          </div>
-          <button type="submit" class="btn w-full bg-primary"
-                        @click.prevent="login" >Sign in</button>
-        </form>
+        <BaseCard>
+          <form>
+            <div class="form-field">
+              <label for="username">Username</label>
+              <input type="text" id="username" v-model.trim="userName" />
+            </div>
+            <div class="form-field">
+              <label for="password">Password</label>
+              <input type="password" id="password" v-model="password" />
+            </div>
+            <button type="submit" class="btn w-full bg-primary"
+                          @click.prevent="login" >Sign in</button>
+          </form>
+        </BaseCard>
       </main>
     </div>
   </div>
